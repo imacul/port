3123a7001e617954aae7298f306197f7307798f1
Emmanuel Oshakpemeh <imacul77@gmail.com>
2024-07-05 08:28:59 +0100

    update footer social links performance

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -45,7 +45,7 @@ const Footer = () => {
               key={info.id}
               className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
             >
-              <Link href={info.href}>
+              <Link href={info.href} target="_blank">
               <Image src={info.img} alt="icons" width={20} height={20} />
               </Link>
             </div>
